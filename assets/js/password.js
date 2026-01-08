a87564ba0629ce3668952c3a90a558efdfddca39
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-09 00:47:44 +0600

    Update password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -7,7 +7,7 @@ function passcheck(){
         return false;
     }
     if (document.getElementById('pass1').value == Password) {
-        window.open("https://masumbillah6778.github.io/cyber-secret-mission/main/assets/html/secret-mission-password-box.html", "_blank");
+        window.open("https://masumbillah6778.github.io/cyber-secret-mission/assets/html/secret-mission-password-box.html", "_blank");
     }
 
 }
